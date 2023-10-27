30720e334a4fae414034462c9bc3ef8cfb74f165
Pankaj Bhadane <pankajb2601@gmail.com>
2023-10-27 22:27:54 +0530

    starting with router project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Link, NavLink } from "react-router-dom";
+// import { Header } from "./components";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+};
+
+export default App;

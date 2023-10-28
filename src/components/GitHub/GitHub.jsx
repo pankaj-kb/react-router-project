e242846a09bb0dad0ab3fa4df8bdd3990fd745f4
Pankaj Bhadane <pankajb2601@gmail.com>
2023-10-28 14:01:03 +0530

    implemented github api

--- src/components/GitHub/GitHub.jsx
+++ src/components/GitHub/GitHub.jsx
@@ -1,10 +1,26 @@
-export default function GitHub() {
-  return(
-  <>
-    <h1>
-        GitHub Stats:
-    </h1>
-    
-  </>
+import { useEffect, useState } from "react";
+
+function GitHub() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/pankaj-kb")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  return (
+    <div
+      className="text-center m-4 
+  bg-gray-400 text-white"
+    >
+      <h1 className="text-3xl">GitHub Followers: {data.followers}</h1>
+      <h1 className="text-3xl">ID: {data.id}</h1>
+      <img className="" src={data.avatar_url} alt="profile pic" width={200} height={200} />
+    </div>
   );
 }
+
+export default GitHub;
